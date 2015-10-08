0de7bb125d9958e0ddb0ae7fbd8322de665d030d
Hein Dauven <heindauven@gmail.com>
2015-10-09 01:40:58 +0200

    Implemented logAction
    The logAction method has been implemented.
    
    A new helper method is introduced called timeFormatting, which will be
    used to add a time before the to log message.

--- Stamboom (start)/src/stamboom/util/userLogging.java
+++ Stamboom (start)/src/stamboom/util/userLogging.java
@@ -1,5 +1,8 @@
 package stamboom.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * userLogging:
  * Class that logs user actions and exceptions caused by invalid
@@ -11,22 +14,36 @@ package stamboom.util;
  */
 public class userLogging {
     
-    private String output;
-    
-    public userLogging() {
-        
-    }
-    
     /**
      * Umbrella method that handles the messages that should be 
      * logged and the actions that check whether the logging file
      * exists and creates it.
      * @return returns the logged message.
+     * @param header marks the type of the logged message
+     * @param message containing information regarding user actions
      */
-    public String logAction() {
-        output = "";
+    public String logAction(String header, String message) {
+        String result = header + ": " + message;
         
-        return output;
+        if (!checkIfFolderExists()) {
+            // TODO: create log folder
+            createNewLogFolder();
+        }
+        
+        if (!checkIfFileExists()) {
+            // TODO: create log file
+            createNewLogFile();
+        }
+        
+        // TODO: write action to file
+        if (writeLogActionToFile()) {            
+            result = timeFormatting() + " " + result;
+            return result;
+        } else {
+            result = "";
+        }
+        
+        return result;
     }
     
     /**
@@ -51,5 +68,30 @@ public class userLogging {
      */
     private boolean createNewLogFile() {
         return false;
-    }    
+    }   
+    
+    /**
+     * Checks whether the log folder exists.
+     * @return whether the folder exists or not.
+     */
+    private boolean checkIfFolderExists() {
+        return false;
+    }
+    
+    /**
+     * Creates a new log folder.
+     * @return whether the new folder is created or not.
+     */
+    private boolean createNewLogFolder() {
+        return false;
+    }
+    
+    /**
+     * Creates a Date for a log, based on the time that the
+     * action is logged.
+     * @return The time when the log is logged.
+     */
+    private String timeFormatting() {
+        return null;
+    } 
 }
